e5c614d6b12962f8452c71fcef6d92a925440f3f
jaraxzus <alex099024@gmail.com>
2023-11-21 17:24:34 +0300

    unable echo from db

--- sql/database.py
+++ sql/database.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=False
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
